9bdf8d8c9c99942d111136e143d046c043279b14
Rahul <mailtorahul724@gmail.com>
2018-07-23 16:24:28 +0530

    jason object parsing

---
+++ TrainingProjects/TrainingProjects/Decerialize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingProjects
+{
+    public class Decerialize
+    {
+        public static void DeserializeObject(Student St)
+        {
+            Student deserializeStd = JsonConvert.DeserializeObject<Student>(St);
+        }
+    }
+}
---
+++ TrainingProjects/TrainingProjects/Serializatio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TrainingProjects
+{
+    public class Serializatio
+    {
+        public static void record(int id, string name)
+        {
+            Student St = new Student(id,name);
+            St.Id = id;
+            St.Name = name;
+        }
+        public static void serialization(Student St)
+        {
+            string json = JsonConvert.SerializeObject(St);
+        }
+
+    }
+}
