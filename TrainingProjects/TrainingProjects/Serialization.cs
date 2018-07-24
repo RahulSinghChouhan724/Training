d04c5443ea026a5813cde0549a6684c49bb45ef6
Rahul <mailtorahul724@gmail.com>
2018-07-24 17:18:50 +0530

    Json parsing with serialization and deserialization

---
+++ TrainingProjects/TrainingProjects/Serialization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace TrainingProjects
+{
+    public class Serialization
+    { 
+        public string serialization(Student s)
+        {
+            string St1 = JsonConvert.SerializeObject(s);
+            dynamic data = JsonConvert.DeserializeObject(St1);
+           // return ((dynamic)JsonConvert.DeserializeObject(St1));
+            return data.Name;
+        }
+
+
+    }
+}
